642b9c66d77b04e5309365f5750ad08af7d5ec00
M. Nur Fawaiq <nurfawaiq@gmail.com>
2021-11-05 14:41:34 +0700

    process calculator

--- js/kalkulator.js
+++ js/kalkulator.js
@@ -10,8 +10,7 @@ function getOutput(){
 function printOutput(num){
     if(num==""){
         document.getElementById("output-values").innerText=num;
-    }
-    else{
+    }else{
         document.getElementById("output-values").innerText=getFormattedNumber(num);
     }
 }
@@ -26,45 +25,44 @@ function getFormattedNumber(num){
 function reverseNumberFormat(num){
     return Number(num.replace(/,/g, ''));
 }
+
 var operator = document.getElementsByClassName("operator");
 for( var i=0;i<operator.length;i++){
     operator[i].addEventListener('click',function(){
-            if(this.id=="clear"){
-                printHistory("");
-                printOutput("");
+        if(this.id=="clear"){
+            printHistory("");
+            printOutput("");
+        }else if(this.id=="backspace"){
+            var
+            output=reverseNumberFormat(getOutput()).toString();
+            if(output){//if output has a value
+                output= output.substr(0,output.length-1);
+            printOutput(output);
             }
-            else if(this.id=="backspace"){
-                var
-                output=reverseNumberFormat(getOutput()).toString();
-                if(output){//if output has a value
-                    output= output.substr(0,output.length-1);
-                printOutput(output);
+        }else{
+            var output = getOutput();
+            var history = getHistory();
+            if(output==""&&history!=""){
+                if(isNaN(history[history.length-1])){
+                    history=history.substr(0,history.length-1);
                 }
             }
-            else{
-                var output = getOutput();
-                var history = getHistory();
-                if(output==""&&history!=""){
-                    if(isNaN(history[history.length-1])){
-                        history=history.substr(0,history.length-1);
-                    }
+            if(output!="" || history!=""){
+                output= output==""?
+                output:reverseNumberFormat(output);
+                history=history+output;
+                if(this.id=="="){
+                    var result= eval(history);
+                    printOutput(result);
+                    printHistory("");
                 }
-                if(output!="" || history!=""){
-                    output= output==""?
-                    output:reverseNumberFormat(output);
-                    history=history+output;
-                    if(this.id=="="){
-                        var result= eval(history);
-                        printOutput(result);
-                        printHistory("");
-                    }
-                    else{
-                        history=history+this.id;
-                        printHistory(history);
-                        printOutput("");
-                    }
+                else{
+                    history=history+this.id;
+                    printHistory(history);
+                    printOutput("");
                 }
             }
+        }
     });
 }
 var number = document.getElementsByClassName("number");
